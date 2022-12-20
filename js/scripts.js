cfbeaa0ff1abf5f320c1c202d3034b660545a637
Phan Tan <pttan1304@gmail.com>
2022-12-20 11:09:37 +0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,18 @@ let hateEl = document.querySelector(".hate");
 
 hateBtn.addEventListener('click', function() {
     hateBtn.style.display = "none";
-    for(let i = 0; i< 10000; i++) {
+    for(let i = 0; i< 10500; i++) {
         let div = document.createElement("div");
         div.classList.add("item");
         div.innerText = "ghét quá";
         hateEl.appendChild(div);
+        if(i == 1306) {
+            div.innerText = "iuuu";
+        } else if (i == 2001) {
+            div.innerText = "dethuong";
+        } else if (i == 9999) {
+            div.innerText = "my embe";
+        }
     }
 
 })
